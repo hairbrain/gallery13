9d3e40df8655c8794a7f9e603595a6a5053f7457
jame <jame.mackson@gmail.com>
2015-03-25 02:18:09 -0500

    added user and events routes and views; added content to application template

---
+++ server/api/models/User.js
@@ -0,0 +1,23 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+
+    firstName : { type: 'string' },
+
+    lastName : { type: 'string' },
+
+    username : { type: 'string' },
+
+    email : { type: 'string' },
+
+    password : { type: 'string' }
+  }
+};
+
