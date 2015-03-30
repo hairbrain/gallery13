836f2719d6befb7368e63494e4203b0038a944ba
jame <jame.mackson@gmail.com>
2015-03-30 15:22:36 -0500

    added ability to create new artists; added stub for editing artist;

--- client/app/router.js
+++ client/app/router.js
@@ -11,12 +11,16 @@ Router.map(function() {
 
   this.resource('event', function() {});
   this.resource('user', function() {});
-  this.route('artists', function() {});
+  this.route('artists', function() {
+    this.route('new');
+  });
   this.resource('artist', { path: '/artist/:artist_slug' }, function() {
     this.route('index', { path:'/'});
     this.route('bio');
     this.route('statement');
     this.route('press');
+    this.route('image', { path: '/image/:image_id' });
+    this.route('edit');
   });
   //this.resource('images', function() {});
 
