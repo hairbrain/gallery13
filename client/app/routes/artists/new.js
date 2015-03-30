92f4b3103eb34437039d79a96c11285dec574607
jame <jame.mackson@gmail.com>
2015-03-30 15:23:43 -0500

    added routes for artist.edit, artist.image, & artists.new

---
+++ client/app/routes/artists/new.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function() {
+    return this.store.createRecord('artist');
+  },
+
+  actions: {
+    createArtist: function(model) {
+      var _this = this;
+      //var userId = this.session.get('user.id');
+      model.save();
+      this.transitionTo('artists');
+      //var user = this.store.find('user', userId).then(function(result) {
+      //    model.set('user', result);
+      //    user = result;
+      //    return _this.geoGoogleService.getLatLongForAddress(model.get('address'));
+      //  }).then(function (response) {
+      //    var latlong = point(response.results[0].geometry.location);
+      //    model.set('location', latlong);
+      //    return model.save();
+      //  }).then(() => this.transitionTo('s.users.user', user));
+    }
+  }
+});
