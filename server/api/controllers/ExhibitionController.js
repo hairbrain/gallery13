d54416f1e33a501cb4ea90d4f397a0740ccf217e
jame <jame.mackson@gmail.com>
2015-04-03 10:36:16 -0500

    WIP on event-manager and exhibition-manager

---
+++ server/api/controllers/ExhibitionController.js
@@ -0,0 +1,11 @@
+/**
+ * ExhibitionsController
+ *
+ * @description :: Server-side logic for managing exhibitions
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
