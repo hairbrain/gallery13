b51a2aa7d26f4560d025bb901653fbef429eb933
jame <jame.mackson@gmail.com>
2015-05-22 12:10:15 -0500

    improved artist editing

---
+++ client/app/routes/admin/artist/edit-exhibition.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function(params) {
+    var exhibition = this.store.find('exhibition', params.exhibition_id);
+    return exhibition;
+  }
+
+});
