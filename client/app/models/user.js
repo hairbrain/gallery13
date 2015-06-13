862d6e3648d87679907fe934fdcb96d6c1646a89
jame <jame.mackson@gmail.com>
2015-06-13 02:22:26 -0500

    added fullName property to user

--- client/app/models/user.js
+++ client/app/models/user.js
@@ -5,5 +5,8 @@ export default DS.Model.extend({
   lastName: DS.attr('string'),
   username: DS.attr('string'),
   //email: DS.attr('string'),
-  password: DS.attr('string')
+  password: DS.attr('string'),
+  fullName: Ember.computed('firstName', 'lastName', function(key, value) {
+    return this.get('firstName') + ' ' + this.get('lastName');
+  })
 });
