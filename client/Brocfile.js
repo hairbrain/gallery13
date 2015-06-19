1c16984b05dfed67e04a40db68a91e22a03694bf
jame <jame.mackson@gmail.com>
2015-06-19 02:09:43 -0500

    removed bootstrap; added semantic-ui for #41

--- client/Brocfile.js
+++ client/Brocfile.js
@@ -19,22 +19,6 @@ var app = new EmberApp();
 
   //import bootstrap
   //app.import('')
-  app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
-  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
-  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {
-    destDir: 'fonts'
-  });
-  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', {
-    destDir: 'fonts'
-  });
-  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {
-    destDir: 'fonts'
-  });
-  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
-    destDir: 'fonts'
-  });
-  app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {
-    destDir: 'fonts'
-  });
+
 
 module.exports = app.toTree();
