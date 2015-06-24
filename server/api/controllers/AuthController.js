97bde7a400fe0bc38a6ff96bc11d2a1bcba59844
jame <jame.mackson@gmail.com>
2015-06-24 01:15:53 -0500

    removed error from merge in AuthController

--- server/api/controllers/AuthController.js
+++ server/api/controllers/AuthController.js
@@ -1,7 +1,7 @@
 var jwt = require('jsonwebtoken');
 var _ = require('lodash');
 var bcrypt = require('bcrypt');
-var User = require('../models/User');
+// var User = require('../models/User');
 
 /**
  * AuthController
