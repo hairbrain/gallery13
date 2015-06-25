356d800c59379854a08868bbce633f354dcf9486
jame <jame.mackson@gmail.com>
2015-06-25 02:27:28 -0500

    partial update to admin/artist/index #41

--- client/app/pods/admin/artist/index/route.js
+++ client/app/pods/admin/artist/index/route.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // model: function(params) {
-  //   var artist = this.store.find("artist", params.artist_id);
-  //   return artist;
-  // }
+  model: function(params) {
+    // var artist = this.store.find("artist", params.artist_id);
+    // return artist;
+    return this.modelFor('admin/artist');
+  }
 });
