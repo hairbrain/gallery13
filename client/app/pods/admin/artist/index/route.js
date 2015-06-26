08826a210ac670616a054e96a85b55282dec4bfb
jame <jame.mackson@gmail.com>
2015-06-26 01:12:42 -0500

    updates to admin.artist route #41

--- client/app/pods/admin/artist/index/route.js
+++ client/app/pods/admin/artist/index/route.js
@@ -4,6 +4,11 @@ export default Ember.Route.extend({
   model: function(params) {
     // var artist = this.store.find("artist", params.artist_id);
     // return artist;
-    return this.modelFor('admin/artist');
+    return this.modelFor('admin.artist');
+    // return this.store
+    // .find('artist', params.artist_id)
+    // .then(function (model) {
+    //   return model.content[0];
+    // });
   }
 });
