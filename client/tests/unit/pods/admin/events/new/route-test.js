11f4f927563f06557c03f148ca4904a9afcad9f4
jame <jame.mackson@gmail.com>
2015-06-27 23:22:34 -0500

    moved admin/events into pods structure #42

---
+++ client/tests/unit/pods/admin/events/new/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:admin/events/new', 'Unit | Route | admin/events/new', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
