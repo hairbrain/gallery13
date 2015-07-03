f51a01a36b73f5d8d64ab90e65963c840746d243
jame <jame.mackson@gmail.com>
2015-07-03 12:39:18 -0500

    updated artist/index with semantic html #41

--- client/app/pods/artists/index/controller.js
+++ client/app/pods/artists/index/controller.js
@@ -12,21 +12,6 @@ export default Ember.Controller.extend({
       this.set('featuredArtistImage', artist.get('featureImage'));
       this.set('featuredArtistCaption', artist.get('statement'));
       return false;
-    },
-    createNew: function () {
-      // this.store.createRecord();
-      // {{debugger}}
-      // var artist = this.store.createRecord('artist', {
-      //   slug: '/new-artist',
-      //   name: 'new',
-      //   statement: '',
-      //   bio: '',
-      //   press: '',
-      //   description: ''//,
-      //   // images: []
-      // });
-      // artist.save();
     }
-
   }
 });
