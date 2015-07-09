9bb8c8957bb62651af14275a5320b7b54edde5ef
jame <jame.mackson@gmail.com>
2015-07-09 02:05:40 -0500

    refinements to artist editing, site layout, post editing, artist card, footer

--- client/app/pods/components/layout-footer/component.js
+++ client/app/pods/components/layout-footer/component.js
@@ -2,5 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'footer',
-  classNames: ['ui', 'row']
+  classNames: ['ui', 'row'] //,
+  //initializeComponent: function(){
+  //  this.$('.ui.sticky')
+  //    .sticky({
+  //      position: 'bottom'
+  //    })
+  //  ;
+  //}.on('didInsertElement')
 });
