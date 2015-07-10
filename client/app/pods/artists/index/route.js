4d6102ac59e2f8b3e7572d44852838bd9021ee85
jame <jame.mackson@gmail.com>
2015-07-10 01:29:37 -0500

    artist updates #41

--- client/app/pods/artists/index/route.js
+++ client/app/pods/artists/index/route.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  titleToken: ''
+  titleToken: '',
+  setupController(controller, model) {
+    //debugger;
+    controller.set('model', model);
+    var firstArtist = model.get('firstObject');
+    controller.set('featuredArtistImage', firstArtist.get('featureImage'));
+    controller.set('featuredArtistCaption', firstArtist.get('statement'));
+  }
 });
