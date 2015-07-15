00027266d9f3d79cd4315a91d7b4280a11f4e1d9
Ian VanSchooten <ian@badgelabsllc.com>
2015-07-14 22:33:32 -0400

    Handle empty models

--- client/app/pods/artists/index/route.js
+++ client/app/pods/artists/index/route.js
@@ -4,9 +4,11 @@ export default Ember.Route.extend({
   titleToken: '',
   setupController(controller, model) {
     //debugger;
-    controller.set('model', model);
-    var firstArtist = model.get('firstObject');
-    controller.set('featuredArtistImage', firstArtist.get('featureImage'));
-    controller.set('featuredArtistCaption', firstArtist.get('statement'));
+    if (model.length > 0) {
+      controller.set('model', model);
+      var firstArtist = model.get('firstObject');
+      controller.set('featuredArtistImage', firstArtist.get('featureImage'));
+      controller.set('featuredArtistCaption', firstArtist.get('statement'));
+    }
   }
 });
