acd8ec615bfce20bfbbc5d49c933be265f89f8f8
jame <jame.mackson@gmail.com>
2015-07-15 03:12:46 -0500

    adds logic to load uploaded image to filepicker as a base64 image string #49

--- client/app/pods/admin/artist/images/new/controller.js
+++ client/app/pods/admin/artist/images/new/controller.js
@@ -13,9 +13,30 @@ export default Ember.ObjectController.extend({
     },
     fileSelected: function(data) {
       var self = this;
+
       data.forEach(function (upload){
-        let image = self.store.createRecord('image', { artist: self.get('model')});
-        image.set('path', upload.url);
+
+        //get a filepicker instance here.
+        filepicker.read(upload, {base64encode: true},
+          function(data) {
+            "use strict";
+            //console.log(data);
+            let image = self.store.createRecord('image', { artist: self.get('model')});
+            image.set('path', 'data:image/jpeg;base64,' + data);
+            image.save();
+          },
+          function(err) {
+            "use strict";
+            console.error(err);
+          },
+          function(progressPercent) {
+            "use strict";
+            //console.log(progressPercent);
+          }
+        );
+
+        //let image = self.store.createRecord('image', { artist: self.get('model')});
+        //image.set('path', upload.url);
       });
       this.send('hidePicker');
     },
